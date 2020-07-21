605bef16022f61554ccd5f252a76769262610188
prithvipsingh <ppsdeora@gmail.com>
2020-07-21 20:00:49 +1000

    updates layouts

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-dark text-white mt-5 pd-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} <a href="https://github.com/starkblaze01/Hostel-Management#team-members">Team Web Tech</a>
+      Copyright &copy; {new Date().getFullYear()} <a href="https://github.com/prithvipsingh/Project3-Udaipur-Hostel">PrithviPSingh</a>
     </footer>
   );
 }
