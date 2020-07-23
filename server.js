5a10d1e0916e27c78e4398efe58e8d5e7bc5e434
prithvipsingh <ppsdeora@gmail.com>
2020-07-23 20:22:09 +1000

    debugging

--- server.js
+++ server.js
@@ -1,19 +1,46 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routers/api/users");
+const students = require("./routers/api/student");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-// Serve up static assets (usually on heroku)
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// connect to mongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Use Routes
+app.use("/api/users", users);
+
+app.use("/api/student", students);
+
+// Server static assets if in production
 if (process.env.NODE_ENV === "production") {
+  //Set static folder
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+const port = process.env.PORT || 5000;
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
