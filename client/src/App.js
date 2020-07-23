543570f1a6487d7b422541124f0a98267fc8d2f2
prithvipsingh <ppsdeora@gmail.com>
2020-07-23 18:39:26 +1000

    modified and updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-//import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -17,12 +17,12 @@ if (localStorage.jwtToken) {
   // decode the token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
   // set user and isAuthenticated
-  //store.dispatch(setCurrentUser(decoded));
+  store.dispatch(setCurrentUser(decoded));
   // check for expired token
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     // logout User
-   // store.dispatch(logoutUser());
+   store.dispatch(logoutUser());
     // redirect to login
     window.location.href = "/login";
   }
