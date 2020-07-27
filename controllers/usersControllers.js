1795c02a509a67e68f2001d87e174d2f5a0f3c3a
prithvipsingh <ppsdeora@gmail.com>
2020-07-27 18:21:08 +1000

    controllers updates

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,34 +1,34 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for UsersController
 module.exports = {
   findAll: function(req, res) {
-    db.Student
+    db.User
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Student
+    db.User
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Student
+    db.User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Student
+    db.User
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Student
+    db.User
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const profileRoutes = require("./profile");
-const roomRoutes = require("./room");
+//const profileRoutes = require("./profile");
+//const roomRoutes = require("./room");
 const staffRoutes = require("./staff");
 const studentRoutes = require("./student");
 const usersRoutes = require("./users");
 // all routes
-router.use("/profile", profileRoutes);
-router.use("/room", roomRoutes);
+//router.use("/profile", profileRoutes);
+//router.use("/room", roomRoutes);
 router.use("/staff", staffRoutes);
 router.use("/student", studentRoutes);
 router.use("/users", usersRoutes);
