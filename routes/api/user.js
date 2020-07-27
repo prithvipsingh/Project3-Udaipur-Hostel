cc13443daeed0153e677445d97363bfb9e82bc74
prithvipsingh <ppsdeora@gmail.com>
2020-07-27 18:43:19 +1000

    updates controllers

---
+++ routes/api/user.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const usersController = require("../../controllers/usersController");
+
+// // Matches with "/api/books"
+router.route("/")
+   .get(usersController.findAll)
+   .post(usersController.create);
+
+
+// router.route("/")
+//   .get(booksController.findAll)
+//   .post(booksController.create);
+
+// // Matches with "/api/books/:id"
+// router
+//   .route("/:id")
+//   .get(booksController.findById)
+//   .put(booksController.update)
+//   .delete(booksController.remove);
+
+module.exports = router;
