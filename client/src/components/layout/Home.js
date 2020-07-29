8b6be3102c58993166ace03cd70339014c988f91
prithvipsingh <ppsdeora@gmail.com>
2020-07-29 17:39:15 +1000

    updates

--- client/src/components/layout/Home.js
+++ client/src/components/layout/Home.js
@@ -16,7 +16,7 @@ class Home extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Hostel-Management</h1>
+                <h1 className="display-3 mb-4">Udaipur-Hostel</h1>
                 <p className="lead"></p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
